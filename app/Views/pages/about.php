4bf6b92e91b184c2d06b32a75ea48487252e922a
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-16 07:56:41 +0700

    Edit file

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,6 +1,16 @@
+<?= $this->extend('layout/templates'); ?>
+
+<?= $this->section('content'); ?>
+
 <div class="container mt-3">
     <h2 class="text-center">Halaman Page</h2>
-    <?php
-    d($didik);
-    ?>
+    <?php foreach ($alamat as $a) : ?>
+        <ul>
+            <li><?= $a['tipe']; ?></li>
+            <li><?= $a['alamat']; ?></li>
+            <li><?= $a['provinsi']; ?></li>
+        </ul>
+    <?php endforeach; ?>
 </div>
+
+<?= $this->endSection(); ?>
