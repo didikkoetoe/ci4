509d9a29e0518b37e8e71696e56be498e6978fba
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-16 07:56:30 +0700

    Edit file dengan teknik baru

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -1,3 +1,8 @@
+<?= $this->extend('layout/templates'); ?>
+
+<?= $this->section('content'); ?>
 <div class="container mt-3">
     <h2 class="text-center">Halaman Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio unde dolores eum eveniet earum voluptate laudantium laborum est, nam alias voluptas voluptates doloribus quam esse itaque magni, optio dignissimos. Ab eligendi non a, sint fuga ipsum aut temporibus sapiente necessitatibus doloribus aliquam sed earum.</p>
 </div>
+<?= $this->endSection(); ?>
