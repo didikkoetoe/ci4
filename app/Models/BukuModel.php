85e2b7453314fcec3f5ff47591bc7df0fc5003b8
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-17 19:49:09 +0700

    Menambahkan allowedFields untuk menambahkan data di database

--- app/Models/BukuModel.php
+++ app/Models/BukuModel.php
@@ -9,6 +9,8 @@ class BukuModel extends Model
     protected $table = 'buku';
     protected $useTimestamps = true;
 
+    protected $allowedFields = ['judul', 'slug', 'penulis', 'penerbit', 'sampul'];
+
     public function getBuku($slug = false)
     {
         if ($slug == false) {
