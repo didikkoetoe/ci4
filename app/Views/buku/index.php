d084f0ff0856b8c44762b0481b08389dd0d3fd4e
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-17 07:30:03 +0700

    Edit form menggunakan database

--- app/Views/buku/index.php
+++ app/Views/buku/index.php
@@ -14,16 +14,19 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>
-                    <img src="/img/atomic.jpg" alt="" class="sampul">
-                </td>
-                <td>Atomic Habit</td>
-                <td>
-                    <a href="" class="btn btn-success">Detail</a>
-                </td>
-            </tr>
+            <?php $i = 1; ?>
+            <?php foreach ($buku as $book) : ?>
+                <tr>
+                    <th scope="row"><?= $i++; ?></th>
+                    <td>
+                        <img src="/img/<?= $book['sampul']; ?>" alt="" class="sampul">
+                    </td>
+                    <td><?= $book['judul']; ?></td>
+                    <td>
+                        <a href="" class="btn btn-success">Detail</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>
